a471b997693c5bc97422ef02a2b91078ec08fb46
GoldenCarrotMLP <77256100+GoldenCarrotMLP@users.noreply.github.com>
2022-12-16 10:44:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-                                                            import pandas as pd
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 colnames = ['CodeNumber','Thickness', 'Size', 'Shape', 'Adhesion', 'Epithetialsize', 'nuclei', 'chromatin', 'nucleoi',
